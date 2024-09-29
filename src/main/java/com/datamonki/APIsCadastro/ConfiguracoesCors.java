928f0fb5868dd606b9b61693beab981ef54031fc
wilker <weslleywilker10@gmail.com>
2024-09-29 20:55:56 -0300

    Alterações

--- src/main/java/com/datamonki/APIsCadastro/ConfiguracoesCors.java
+++ src/main/java/com/datamonki/APIsCadastro/ConfiguracoesCors.java
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-package com.datamonki.APIsCadastro; import org.springframework.context.annotation.Bean; import 
-org.springframework.context.annotation.Configuration; import 
-org.springframework.web.servlet.config.annotation.CorsRegistry; import 
-org.springframework.web.servlet.config.annotation.WebMvcConfigurer; @Configuration public class 
-
-ConfiguracoesCors {
-    @Bean public WebMvcConfigurer corsConfigurer() { return new WebMvcConfigurer() { @Override public void 
-            addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // Configura todos os endpoints que começam com /api/ 
-                        .allowedOrigins("http://localhost:5174") // Permite a origem do React 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos 
-                        .allowedHeaders("*") // Permite todos os cabeçalhos .allowCredentials(true); // Permite 
-                        o envio de cookies (se necessário)
-            }
-        };
-    }
-}
-=======
 package com.datamonki.APIsCadastro;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -40,4 +21,4 @@ public class ConfiguracoesCors {
         };
     }
 }
->>>>>>> 78e413ebd08dfe0c2b405a96df4b530cca19f178
+
